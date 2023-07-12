9a198306b742ea06c1a17309174af567c8bb1dab
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-07-12 22:19:34 +0530

    deploy

--- index.js
+++ index.js
@@ -3,22 +3,26 @@ const errorHandler = require('./middlewares/errorHandler');
 const connectDB = require("./db/connect");
 const branchRoutes = require('./routes/branchRoutes');
 const subRoutes = require('./routes/subRoutes');
-const modRoutes =  require('./routes/modRoutes');
+const modRoutes = require('./routes/modRoutes');
 const userRoutes = require('./routes/userRoutes');
 const noteRoutes = require('./routes/noteRoutes');
+const cors = require('cors');
 require('dotenv').config();
+
+
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors())
 const start = async () => {
-    try{
+    try {
         await connectDB(process.env.MONGO_URI);
-        app.listen(port,()=>{
+        app.listen(port, () => {
             console.log(`Server started on ${port}`)
         });
-    }catch(err){
+    } catch (err) {
         console.log(err);
     }
 };
@@ -26,8 +30,8 @@ const start = async () => {
 app.use('/api/v1/branch', branchRoutes);
 app.use('/api/v1/sub', subRoutes);
 app.use('/api/v1/mod', modRoutes);
-app.use('/api/v1/users',userRoutes);
-app.use('/api/v1/notes',noteRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/notes', noteRoutes);
 app.use(errorHandler);
 start();
 
