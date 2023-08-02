40ca8b039b64ccc60f867cb20b2207182e25c4cd
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-08-03 02:35:09 +0530

    transfercoins

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const Router = require('express');
 const router = Router();
-const { userInfo, registerUser, loginUser, verifyemail, sendResetPasswordEmail, resetPassword } = require('../controllers/userCntrl');
+const { userInfo, registerUser, loginUser, verifyemail, sendResetPasswordEmail, resetPassword, transferCoins } = require('../controllers/userCntrl');
 const validateToken = require('../middlewares/validateToken');
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser);
@@ -8,6 +8,7 @@ router.route('/emailverify/:tokenId').get(verifyemail);
 router.route('/get_user_info').get(validateToken, userInfo);
 router.route('/reset_email').post(sendResetPasswordEmail);
 router.route('/reset_password').post(resetPassword);
+router.route('/transfer_coins/:receiverID').post(validateToken, transferCoins);
 
 
 module.exports = router;
