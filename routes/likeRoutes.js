42a535554730d73540c3d5417ac15e528561172c
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-09 02:49:37 +0530

    like done

---
+++ routes/likeRoutes.js
@@ -0,0 +1,12 @@
+const Router = require('express');
+const { likeUnlikeNote } = require('../controllers/likeCntrl')
+const validateToken = require('../middlewares/validateToken')
+const router = Router();
+
+
+router.post('/likeUnlikeNote/:noteId', validateToken, likeUnlikeNote)
+
+
+
+
+module.exports = router;
