09754cdbe9f13548f3b16e90c35fdf12d30a53cd
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-10-18 21:16:32 +0530

    email changed

--- utils/EmailTemplates.js
+++ utils/EmailTemplates.js
@@ -34,7 +34,7 @@ const successFullVerification = () => {
   <div style="background-color: #f0f0f0; padding: 20px; border-radius: 8px;">
     <p style="font-size: 16px; font-family: 'Arial', sans-serif; color: #444; text-align: center;">You have successfully verified your email.</p>
     <div style="text-align: center; margin-top: 20px;">
-      <a href="http://localhost:3000/login" style="display: inline-block; background-color: #008080; color: #fff; font-size: 18px; font-family: 'Arial', sans-serif; text-decoration: none; padding: 10px 20px; border-radius: 5px; border: 2px solid #008080; transition: background-color 0.3s ease-in-out;">
+      <a href="https://www.noteshare.online/login" style="display: inline-block; background-color: #008080; color: #fff; font-size: 18px; font-family: 'Arial', sans-serif; text-decoration: none; padding: 10px 20px; border-radius: 5px; border: 2px solid #008080; transition: background-color 0.3s ease-in-out;">
         Go to Home Page
       </a>
     </div>
